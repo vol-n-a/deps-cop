b98a16bdc156534a25c7518ca6558a49f4ea9ce2
vol-n-a <volokhov.nik@yandex.ru>
2025-02-14 01:31:00 +0300

    perf: Parallelize recent checker execution

--- src/checkers/semver-checker.ts
+++ src/checkers/semver-checker.ts
@@ -4,6 +4,37 @@ import { satisfies } from "semver";
 import type { DependencyMap } from "../utils/get-dependency-map";
 import type { SemverRules } from "../utils/get-depscop-config";
 
+const checkSemverRulesEntry = (
+  dependencyMap: DependencyMap,
+  [dependency, [version, reason]]: [string, [string, string]]
+): void => {
+  const dependencyValue = dependencyMap.get(dependency);
+
+  // If the dependency from config is not installed, skip it
+  if (!dependencyValue) {
+    return;
+  }
+
+  // If the dependency from config satisfies the rule, skip
+  if (
+    dependencyValue.rootVersion &&
+    satisfies(dependencyValue.rootVersion, version)
+  ) {
+    return;
+  }
+
+  // Report the error
+  console.log(
+    chalk.red(
+      `semver: ${dependency}@${
+        dependencyValue.rootVersion
+      } does not satisfy ${dependency}@${version}\n\tReason: ${chalk.italic(
+        reason
+      )}`
+    )
+  );
+};
+
 /**
  * Validates semver based depscop rules
  *
@@ -14,31 +45,7 @@ export const semverChecker = (
   dependencyMap: DependencyMap,
   semverRules: SemverRules
 ): void => {
-  for (const [dependency, [version, reason]] of Object.entries(semverRules)) {
-    const dependencyValue = dependencyMap.get(dependency);
-
-    // If the dependency from config is not installed, skip it
-    if (!dependencyValue) {
-      continue;
-    }
-
-    // If the dependency from config satisfies the rule, skip
-    if (
-      dependencyValue.rootVersion &&
-      satisfies(dependencyValue.rootVersion, version)
-    ) {
-      continue;
-    }
-
-    // Report the error
-    console.log(
-      chalk.red(
-        `semver: ${dependency}@${
-          dependencyValue.rootVersion
-        } does not satisfy ${dependency}@${version}\n\tReason: ${chalk.italic(
-          reason
-        )}`
-      )
-    );
-  }
+  Object.entries(semverRules).map((semverRulesEntry) =>
+    checkSemverRulesEntry(dependencyMap, semverRulesEntry)
+  );
 };
