69eab246594ea9f26fcbf66b6d450f3072b66186
vol-n-a <volokhov.nik@yandex.ru>
2025-02-19 23:36:03 +0300

    feat: Added terminal task list

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { Listr } from "listr2";
+
 import { forbiddenChecker } from "./checkers/forbidden-checker";
 import { recentChecker } from "./checkers/recent-checker";
 import { semverChecker } from "./checkers/semver-checker";
@@ -13,13 +15,24 @@ getDependencyTree()
       new Map([...res.entries()].filter(([_dep, entry]) => entry.rootVersion))
   )
   .then((res) => Promise.all([res, getDepscopConfig()]))
-  .then(([dependencyMap, { forbidden, recent, semver }]) =>
-    Promise.all([
-      forbiddenChecker(dependencyMap, forbidden),
-      recentChecker(dependencyMap, recent),
-      semverChecker(dependencyMap, semver),
-    ])
-  )
+  .then(([dependencyMap, { forbidden, recent, semver }]) => {
+    const listr = new Listr([
+      {
+        title: "Forbidden rules check",
+        task: () => forbiddenChecker(dependencyMap, forbidden),
+      },
+      {
+        title: "Recent rules check",
+        task: async () => recentChecker(dependencyMap, recent),
+      },
+      {
+        title: "Semver rules check",
+        task: () => semverChecker(dependencyMap, semver),
+      },
+    ]);
+
+    return listr.run().catch(console.error);
+  })
   .then(() => {
     console.log(stats.getInfo());
   });
