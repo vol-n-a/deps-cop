85f5b376fc5ce451e98a5f9ee3af647d6e58d732
vol-n-a <volokhov.nik@yandex.ru>
2025-03-22 18:13:14 +0300

    feat: `commander` for cli building

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,13 @@
-import { Listr } from "listr2";
+import { program } from "commander";
 
-import { forbiddenChecker } from "./checkers/forbidden-checker";
-import { recentChecker } from "./checkers/recent-checker";
-import { semverChecker } from "./checkers/semver-checker";
-import { stats } from "./stats/stats";
-import { getDependencyMap } from "./utils/get-dependency-map";
-import { getDepscopConfig } from "./utils/get-depscop-config";
-import { getDependencyTree } from "./utils/npm/get-dependency-tree";
+import packageJSON from "../package.json";
+import { runCheckers } from "./checkers";
 
-getDependencyTree()
-  .then(getDependencyMap)
-  .then(
-    (res) =>
-      new Map([...res.entries()].filter(([_dep, entry]) => entry.rootVersion))
-  )
-  .then((res) => Promise.all([res, getDepscopConfig()]))
-  .then(([dependencyMap, { forbidden, recent, semver }]) => {
-    const listr = new Listr(
-      [
-        {
-          title: "Forbidden rules check",
-          task: () => forbiddenChecker(dependencyMap, forbidden),
-        },
-        {
-          title: "Recent rules check",
-          task: async () => recentChecker(dependencyMap, recent),
-        },
-        {
-          title: "Semver rules check",
-          task: () => semverChecker(dependencyMap, semver),
-        },
-      ],
-      {
-        concurrent: true,
-      }
-    );
+program
+  .name("deps-cop")
+  .version(packageJSON.version)
+  .description("DepsCop - whitelist for package.json dependencies");
 
-    return listr.run().catch(console.error);
-  })
-  .then(() => {
-    stats.print();
-  });
+program.parse();
+
+void runCheckers();
