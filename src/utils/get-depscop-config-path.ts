c912c2fc5c8fed74bc0ae7bf2a332bb582925b12
vol-n-a <volokhov.nik@yandex.ru>
2025-05-08 15:15:27 +0300

    fix: DepsCop config in .ts-files are read correctly

--- src/utils/get-depscop-config-path.ts
+++ src/utils/get-depscop-config-path.ts
@@ -1,6 +1,5 @@
 import { access } from "node:fs/promises";
 import path from "node:path";
-import { pathToFileURL } from "node:url";
 
 const CONFIG_BASENAME = "depscop.config";
 const EXTENSIONS_PRIORITY = [
@@ -14,40 +13,40 @@ const EXTENSIONS_PRIORITY = [
 ];
 
 /**
- * Asynchronously locates and returns the URL of the first accessible `depscop.config.*` file
+ * Asynchronously locates and returns the path of the first accessible `depscop.config.*` file
  * in the current working directory, based on a predefined priority of file extensions.
  *
  * The function looks for configuration files with the following extensions (in order):
  * `.json`, `.ts`, `.mts`, `.cts`, `.js`, `.mjs`, `.cjs`. All files are checked in parallel using
  * non-blocking file system access, and the first file that exists and is accessible is returned
- * as a `file://` URL.
+ * as a filesystem path.
  *
- * @returns A promise that resolves to the `file://` URL of the first accessible configuration file.
+ * @returns A promise that resolves to the filesystem path of the first accessible configuration file.
  *
  * @throws If no configuration file is found or none of them is accessible. The error message includes the list of tried filenames.
  */
-export const getDepscopConfigUrl = async (): Promise<string> => {
-  const possibleUrls = EXTENSIONS_PRIORITY.map((ext) =>
+export const getDepscopConfigPath = async (): Promise<string> => {
+  const possiblePaths = EXTENSIONS_PRIORITY.map((ext) =>
     path.resolve(process.cwd(), `${CONFIG_BASENAME}${ext}`)
   );
 
-  const accessibleUrls = possibleUrls.map(async (possibleUrl) => {
+  const accessiblePaths = possiblePaths.map(async (possiblePath) => {
     try {
-      await access(possibleUrl);
-      return possibleUrl;
+      await access(possiblePath);
+      return possiblePath;
     } catch {
       return null;
     }
   });
 
-  const urls = await Promise.all(accessibleUrls);
+  const paths = await Promise.all(accessiblePaths);
 
-  const firstUrl = urls.find(Boolean);
-  if (!firstUrl) {
+  const firstPath = paths.find(Boolean);
+  if (!firstPath) {
     throw new Error(
       `No config file found or none of them is not accessible. Accepted config files: ${EXTENSIONS_PRIORITY.map((ext) => `${CONFIG_BASENAME}${ext}`).join(", ")}`
     );
   }
 
-  return pathToFileURL(firstUrl).href;
+  return firstPath;
 };
