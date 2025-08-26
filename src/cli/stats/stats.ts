b8b84aa4a7e6c0db63927964a0a2ae4f373c30ea
vol-n-a <volokhov.nik@yandex.ru>
2025-08-26 23:50:57 +0300

    chore: Fix imports

--- src/cli/stats/stats.ts
+++ src/cli/stats/stats.ts
@@ -1,6 +1,7 @@
 import chalk from "chalk";
 
-import { Severity } from "../../config/index.js";
+import { Severity } from "src/config/index.js";
+
 import type { CliOptions } from "../model/index.js";
 import type { RuleViolation } from "./model/index.js";
 
