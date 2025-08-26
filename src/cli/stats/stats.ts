81185579ce1bf3820bc9f9227fc56411efc695ca
vol-n-a <volokhov.nik@yandex.ru>
2025-08-26 23:48:53 +0300

    refactor: Add `src/cli/stats/model` folder

--- src/cli/stats/stats.ts
+++ src/cli/stats/stats.ts
@@ -2,7 +2,7 @@ import chalk from "chalk";
 
 import { Severity } from "../../config/index.js";
 import type { CliOptions } from "../model/index.js";
-import type { RuleViolation } from "./rule-violation.js";
+import type { RuleViolation } from "./model/index.js";
 
 class Stats {
   #cliOptions: CliOptions | null = null;
