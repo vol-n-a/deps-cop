4ade464167588b5def965528bde9044e6a673680
vol-n-a <volokhov.nik@yandex.ru>
2025-09-05 21:30:19 +0300

    test: Exclude `build` folder from coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from "vitest/config";
+import { coverageConfigDefaults, defineConfig } from "vitest/config";
 import path from "node:path";
 
 export default defineConfig({
   test: {
     environment: "node",
+    coverage: {
+      exclude: ["build/**", ...coverageConfigDefaults.exclude],
+    },
   },
   resolve: {
     alias: {
