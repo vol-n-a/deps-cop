42e97af5fc144f5d145a888274485037f03b3206
vol-n-a <volokhov.nik@yandex.ru>
2025-09-18 09:48:25 +0300

    chore: Rename `readJavaScriptConfig` -> `importJSModuleDefaultExport`

--- src/config/utils/read-depscop-config/import-js-module-default-export.ts
+++ src/config/utils/read-depscop-config/import-js-module-default-export.ts
@@ -6,7 +6,9 @@ import { pathToFileURL } from "node:url";
  * @param path - Path to the JavaScript configuration file
  * @returns The default export from the file
  */
-export const readJavaScriptConfig = async (path: string): Promise<unknown> => {
+export const importJSModuleDefaultExport = async (
+  path: string
+): Promise<unknown> => {
   const url = pathToFileURL(path).href;
   return (await import(url)).default;
 };
